148f1edfa59d271042b6ee263bae71b290bab247
divya <tejaroyal903@gmail.com>
2026-08-30 19:05:20 +0530

    Add day_03 variables

---
+++ day_3/variables.py
@@ -0,0 +1,34 @@
+#variables
+a = 10
+b = 20
+
+temp = a
+a = b
+b = temp
+
+print("a =", a)
+print("b =", b)
+ # with out variables
+a = 10
+b = 20
+
+a = a + b
+b = a - b
+a = a - b
+
+print(a, b)
+
+# swapping
+a = 10
+b = 20
+a, b = b, a
+print(a, b)
+
+# variables
+
+age = 22
+name = "Harish"
+percentage = 95.5
+print(age)
+print(name)
+print(percentage)
