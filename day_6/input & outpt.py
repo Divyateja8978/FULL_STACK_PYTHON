7c27360854e77ff709f1603cb54eefe67c8ab343
divya <tejaroyal903@gmail.com>
2026-08-31 14:28:38 +0530

    Update day_06 input and output

---
+++ day_6/input & outpt.py
@@ -0,0 +1,9 @@
+# input formatting
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+salary = float(input("Enter your salary: "))
+
+print("Name:", name)
+print("Age:", age)
+print("Salary:", salary)
+
